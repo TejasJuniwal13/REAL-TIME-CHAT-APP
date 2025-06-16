56b58da731b4f07f642c64d1cd5b924c56e94cfb
TejasJunwial13 <tejasjuniwal13@gmail.com>
2025-06-17 03:18:21 +0530

    Connected the frontend via backend

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,17 +1,20 @@
 // // postcss.config.js
 // export default {
 //   plugins: {
-//     '@tailwindcss/postcss': {},
+//     tailwindcss: {},
 //     autoprefixer: {},
-//   }
+//   },
 // }
 
 
 
 // postcss.config.js
-module.exports = {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-  },
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
+export default {
+  plugins: [
+    tailwindcss(),
+    autoprefixer(),
+  ],
 }
