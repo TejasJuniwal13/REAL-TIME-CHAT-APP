78c8407e621a6c10b702bd881f9295da719e77d1
TejasJunwial13 <tejasjuniwal13@gmail.com>
2025-06-17 03:19:06 +0530

    Setup the cors

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const connectDB = require('./db/connect');
 const userRoutes = require('./routes/user.routes')
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const app = express();
 
@@ -11,6 +12,7 @@ connectDB();
 
 
 //middlewares
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
