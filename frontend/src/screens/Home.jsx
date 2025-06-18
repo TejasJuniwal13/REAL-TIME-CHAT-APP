a71573bceb5b7fe1bca8b943fb4ccef6789263b5
TejasJunwial13 <tejasjuniwal13@gmail.com>
2025-06-18 22:17:18 +0530

    Created the Home component

---
+++ frontend/src/screens/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import { UserContext } from '../context/user.context'
+// import { useContext } from 'react'
+
+
+const Home = () => {
+
+    // const {user} = useContext(UserContext);
+    
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
