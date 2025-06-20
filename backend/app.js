ed82d6cde7228ce873eed40a79617d16bdd55305
TejasJunwial13 <tejasjuniwal13@gmail.com>
2025-06-20 14:04:19 +0530

    Added the project middleware

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const connectDB = require('./db/connect');
 const userRoutes = require('./routes/user.routes')
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-
+const projectRoutes = require('./routes/project.routes');
 const app = express();
 
 connectDB();
@@ -21,6 +21,7 @@ app.use(cookieParser());
 
 //routes
 app.use('/user', userRoutes);
+app.use('/projects', projectRoutes);
 app.get('/',(req,res)=>{
     res.send('WELCOME TO APP')
 })
