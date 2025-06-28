5c759a1a3a76d486d84dde8b1b66ea9ea0cf5ea2
TejasJunwial13 <tejasjuniwal13@gmail.com>
2025-06-28 14:30:00 +0530

    Created the project route

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import Login from '../screens/Login'
 import Register from '../screens/Register'
 import Home from '../screens/Home'
+import Project from '../screens/Project'
 
 const AppRoutes = () => {
   return (
@@ -10,7 +11,8 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/register" element={<Register />}></Route>
-        <Route path="login" v element={<Login />}></Route>
+        <Route path="/login"  element={<Login />}></Route>
+        <Route path="/project" element={<Project/>}></Route>
       </Routes>
     </BrowserRouter>
   )
